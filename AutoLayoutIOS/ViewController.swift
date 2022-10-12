ba1eabf8560da78b9753aeef3f647ad3047927ba
oluwasegun <ilorioluwasegun@gmail.com>
2022-10-12 13:40:21 +0100

    Created auto layout for a dice rolling screen to fit different screen sizes

--- AutoLayoutIOS/ViewController.swift
+++ AutoLayoutIOS/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var rightDiceNumber = 0
+    var leftDiceNumber = 0
+    let diceImages =  ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func roleButtonPressed(_ sender: UIButton) {
+        leftDiceNumber = Int.random(in: 0...5)
+        rightDiceNumber = Int.random(in: 0...5)
+        diceImageViewRight.image = UIImage(named: diceImages[rightDiceNumber])
+        diceImageViewLeft.image = UIImage(named: diceImages[leftDiceNumber])
+        
+    }
 }
-
